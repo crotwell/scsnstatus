385a22e353030150768939d16958aaeb50a5bdcd
Philip Crotwell <crotwell@seis.sc.edu>
2017-11-17 16:11:47 -0500

    move model to index route

--- app/routes/stations/index.js
+++ app/routes/stations/index.js
@@ -1,4 +1,31 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
+import seisplotjs from 'ember-seisplotjs';
+
+const networkCode = 'CO';
+const ringserver_host = 'eeyore.seis.sc.edu';
+const ringserver_port = 6382;
 
 export default Route.extend({
+  model: function(params) {
+    return RSVP.hash({
+      network: this.store.findRecord('network', networkCode),
+      latency: this.store.query('stream-status',
+       {host: ringserver_host,
+        port: ringserver_port,
+        match: '^'+networkCode+'_.*'
+      })
+    });
+  },
+    afterModel: function(model, transition) {
+      console.log("station.index afterModel");
+      let out = RSVP.hash({
+        stationHash: model.network.get('stations')
+      });
+      return out.then(hash => {
+        console.log("afterModel RSVP hash "+model.network.get('stations'));
+          console.log("afterModel RSVP hash "+model.network.get('stations').get('length'));
+        return hash;
+      });
+    }
 });

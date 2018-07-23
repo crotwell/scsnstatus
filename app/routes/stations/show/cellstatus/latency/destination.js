1237bf3e56a8ab05365b6826d003c1cf8caeba22
Philip Crotwell <crotwell@seis.sc.edu>
2018-07-23 14:59:55 -0400

    add action on latency/destination

--- app/routes/stations/show/cellstatus/latency/destination.js
+++ app/routes/stations/show/cellstatus/latency/destination.js
@@ -10,7 +10,26 @@ export default Route.extend({
       station: parentModel.station,
       stationList: parentModel.stationList,
       cellstatus: parentModel.cellstatus,
-      destination: params.destination
+      destination: params.destination ? params.destination : 'eeyore'
     };
   },
+  actions: {
+    changeStationOnRoute(station_id) {
+      const routeName = this.get('routeName');
+      const routeA = routeName.split('.');
+      let destination = this.get('model').destination;
+      if ( ! destination) {
+        destination = 'eeyore';
+        this.get('model').destination = destination;
+      }
+        console.log(routeName+" route changeStation destination.js: "+station_id+" dest:"+this.get('model').destination);
+        console.log("routeA: "+routeA);
+      return this.transitionTo('stations.show.cellstatus.latency.destination',
+                                station_id,
+                                destination,
+                                { "queryParams": {
+                                  "end": moment(this.get('model').end).format("YYYY-MM-DD"),
+                                  "days": this.get('model').days }});
+    }
+  }
 });

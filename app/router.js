0e2c026ecd8ef4966ed824ea8fdf1835a70f0f53
Philip Crotwell <crotwell@seis.sc.edu>
2020-06-26 09:05:06 -0400

    start over ember 3.17

--- app/router.js
+++ app/router.js
@@ -1,44 +1,10 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
-const Router = EmberRouter.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
-});
+export default class Router extends EmberRouter {
+  location = config.locationType;
+  rootURL = config.rootURL;
+}
 
 Router.map(function() {
-  this.route('stations', function() {
-    this.route('show', {
-      path: ':station_id'
-    }, function() {
-      this.route('cellstatus', function() {
-        this.route('voltage');
-        this.route('rssi');
-        this.route('latency', function() {
-          this.route('destination', {
-            path: ':destination'
-          });
-        });
-      });
-
-      this.route('channels', function() {
-        this.route('show', {
-          path: ':channel_id'
-        }, function() {});
-      });
-      this.route('helicorder');
-    });
-  });
-  this.route('quakes', function() {
-    this.route('detail', {
-      path: ':quake_id'
-    });
-  });
-  this.route('global-quakes', function() {
-    this.route('detail', {
-      path: ':quake_id'
-    });
-  });
 });
-
-export default Router;

ecd54c9b97c7d1f862eb8cae44e05bce663f38e1
Philip Crotwell <crotwell@seis.sc.edu>
2020-09-01 15:02:47 -0400

    cache 100 values

--- app/services/cell-status.js
+++ app/services/cell-status.js
@@ -16,26 +16,52 @@ import {
 export default class CellStatusService extends Service {
 
   baseURL = 'http://eeyore.seis.sc.edu/earthworm/cell-stats/';
+  cache = [];
+  maxCacheLength = 100;
 
-  queryCellStatus(station, year, jday) {
+  queryCellStatus(station, year, dayofyear) {
     const mythis = this;
-    let url = `${this.baseURL}/${year}/${jday}/${station}.json`;
+    const today = moment.utc();
+    if (year !== today.year() || dayofyear !== today.dayOfYear()) {
+      // only look in cache if not "today" so we get updates
+      // careful of int vs string with ===
+      const yearStr = ""+year;
+      const dayofyearStr = ""+dayofyear;
+      let cachedValue = this.cache.find( cellStat => {
+        return cellStat.dayofyear === dayofyearStr
+          && cellStat.station === station && cellStat.year === yearStr;
+      });
+      if (cachedValue) {
+        return new RSVP.Promise(function(resolve, reject) {
+          resolve(cachedValue);
+        });
+      }
+    }
+    let url = `${this.baseURL}${year}/${dayofyear}/${station}.json`;
     return util.doFetchWithTimeout(url, null, 10)
-    .then(out => {
-        console.log(`queryCellStatus got ${out}`);
+      .then(out => {
         return out.json();
+      }).then(json => {
+        if (year !== today.year() || dayofyear !== today.dayOfYear()) {
+          // only push non-today
+          mythis.cache.push(json);
+        }
+        while (mythis.cache.length > mythis.maxCacheLength) {
+          mythis.cache.shift();
+        }
+        return json;
       }, function(error){
         console.log(error);
         console.log("...returning valid but empty value.")
-        return mythis.emptyCellStatus(station, year, jday);
+        return mythis.emptyCellStatus(station, year, dayofyear);
       });
 
   }
 
-  emptyCellStatus(station, year, jday) {
+  emptyCellStatus(station, year, dayofyear) {
     return {
       "station": station,
-      "dayofyear": jday,
+      "dayofyear": dayofyear,
       "values": [],
       "year": year
     };

07e4c359889c78ea125ce15b1a6424fb80ac271a
Philip Crotwell <crotwell@seis.sc.edu>
2020-09-24 09:38:27 -0400

    check for case where nothing returned for latency

--- app/helpers/latency-seriousness.js
+++ app/helpers/latency-seriousness.js
@@ -7,17 +7,22 @@ const breakpoints = {
   bad: moment.duration(1, 'hour')
 };
 
-export default helper(function latencySeriousness([endtime]/*, hash*/) {
-  if (! endtime) {return "unknown"};
+const LATENCY_GOOD = "latencygood";
+const LATENCY_WORRY = "latencyworry";
+const LATENCY_BAD = "latencybad";
+const LATENCY_BADBAD = "latencybadbad";
 
-  const dur = moment.duration(moment.utc().diff(endtime));
-  let out = "latencybadbad";
-  if (dur.asMilliseconds() < breakpoints.good.asMilliseconds()) {
-    out = "latencygood";
-  } else if (dur.asMilliseconds() < breakpoints.worry.asMilliseconds()) {
-    out = "latencyworry";
-  } else if (dur.asMilliseconds() < breakpoints.bad.asMilliseconds()) {
-    out = "latencybad";
-  }
+export default helper(function latencySeriousness([endtime]/*, hash*/) {
+  let out = LATENCY_BADBAD;
+  if (endtime) {
+    const dur = moment.duration(moment.utc().diff(endtime));
+    if (dur.asMilliseconds() < breakpoints.good.asMilliseconds()) {
+      out = LATENCY_GOOD;
+    } else if (dur.asMilliseconds() < breakpoints.worry.asMilliseconds()) {
+      out = LATENCY_WORRY;
+    } else if (dur.asMilliseconds() < breakpoints.bad.asMilliseconds()) {
+      out = LATENCY_BAD;
+    }
+  }  
   return out;
 });

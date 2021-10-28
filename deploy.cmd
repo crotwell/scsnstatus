58d931744d820c33b020375f6189009f7ebc5dd1
Philip Crotwell <crotwell@seis.sc.edu>
2021-10-28 13:00:22 -0400

    better deploy cmd

--- deploy.cmd
+++ deploy.cmd
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-ember build --environment=production && scp -r dist/* scsn@eeyore.seis.sc.edu:/data/scsn/www/scsnstatus.new/.
+# this causes script to exit if any subcommand returns non-zero error code
+set -e
+
+ember build --environment=production
+scp -r dist scsn@eeyore.seis.sc.edu:/data/scsn/www/scsnstatus.new
+scp -r dist scsn@thecloud.seis.sc.edu:/data/scsn/www/scsnstatus.new

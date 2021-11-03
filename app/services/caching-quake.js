fb1b5ba57deee0cbdac06f5e8bbe71af9c5446b9
Philip Crotwell <crotwell@seis.sc.edu>
2021-11-03 13:21:10 -0400

    move old endtime to the future, bug with recent eq not showing up. Use text xml file on webserver instead of query USGS for speed for old events

--- app/services/caching-quake.js
+++ app/services/caching-quake.js
@@ -2,6 +2,8 @@ import Service from '@ember/service';
 import { inject as service } from '@ember/service';
 import RSVP from 'rsvp';
 import seisplotjs from 'seisplotjs';
+import dataHost from '../utils/data-host';
+import FdsnEventSerializer from 'ember-seisplotjs/serializers/fdsnevent';
 
 export default class CachingQuakeService extends Service {
   @service store;
@@ -17,7 +19,8 @@ export default class CachingQuakeService extends Service {
   updateInterval = moment.duration(5, "minutes");
   lagDays = 30;
   netStart = moment.utc('2009-06-01T00:00:00');
-  oldLocalEnd = moment.utc('2021-10-30T00:00:00');
+  oldLocalEnd = moment.utc('2021-11-30T00:00:00');
+  historicalUrl = "/scsn/sc_quakes/sc_quakes.xml";
   appModel = {
     networkCode: 'CO',
     SCBoxArea: {
@@ -46,27 +49,24 @@ export default class CachingQuakeService extends Service {
   loadLocal() {
     const that = this;
     const end = moment.utc();
-    let loadingPromise = this.loadOldLocal();
-    if (this.cache.updateTime === null || moment.utc().subtract(this.updateInterval).isAfter(this.cache.updateTime)) {
-      loadingPromise.then(oldLocal => {
-        console.log(`oldLocal len: ${oldLocal} `);
+    return this.loadOldLocal().then(oldLocal => {
+      if (this.cache.updateTime === null || moment.utc().subtract(this.updateInterval).isAfter(this.cache.updateTime)) {
         return RSVP.hash({
           oldLocal: oldLocal,
           recentLocal: this.store.query('quake', this.createLocalQueryParams(this.oldLocalEnd, end))
+        }).then(hash => {
+          const out = Ember.A();
+          hash.oldLocal.forEach(q => out.push(q));
+          hash.recentLocal.forEach(q => out.push(q));
+          return out;
+        }).then(locals => {
+          that.cache.local = locals;
+          return locals;
         });
-      }).then(hash => {
-        const out = Ember.A();
-        hash.oldLocal.forEach(q => out.push(q));
-        hash.recentLocal.forEach(q => out.push(q));
-        return out;
-      }).then(locals => {
-        that.cache.local = locals;
-        return locals;
-      });
-    } else {
-      loadingPromise = RSVP.all( this.cache.local );
-    }
-    return loadingPromise;
+      } else {
+        return RSVP.resolve( this.cache.local );
+      }
+    });
   }
 
   load(start, end) {
@@ -74,9 +74,6 @@ export default class CachingQuakeService extends Service {
     if ( ! seisplotjs.util.isDef(end)) {end = moment.utc();}
     if ( ! seisplotjs.util.isDef(start)) {start = moment.utc(end).subtract(this.lagDays, 'days');}
     if (start.isBefore(this.cache.cacheStart) || end.isAfter(moment.utc(this.cache.cacheEnd).add(this.updateInterval))) {
-console.log(`Reload due to start/end: cache: ${this.cache.cacheStart}/${this.cache.cacheEnd}  load: ${start}/${end}`)
-console.log(`start ${start.isBefore(this.cache.cacheStart)}`)
-console.log(`end: ${end.isAfter(moment.utc(this.cache.cacheEnd).add(this.updateInterval))}`)
       this.cache.updateTime = null; // force load
     }
     if (this.cache.updateTime === null || moment.utc().subtract(this.updateInterval).isAfter(this.cache.updateTime)) {
@@ -97,7 +94,6 @@ console.log(`end: ${end.isAfter(moment.utc(this.cache.cacheEnd).add(this.updateI
     });
   }
   _internalLoad(start, end) {
-    console.log(`_internalLoad ${start.toISOString()}  ${end.toISOString()}`)
     const that = this;
     let hash = {
       globalQueryParams: this.createGlobalQueryParams(start, end),
@@ -116,7 +112,6 @@ console.log(`end: ${end.isAfter(moment.utc(this.cache.cacheEnd).add(this.updateI
       that.cache.global = hash.globalQuakeList;
       that.cache.regional = hash.regionalQuakeList;
       that.cache.local = hash.localQuakeList;
-      console.log(`_internalLoad(start, end) gl: ${hash.globalQuakeList.length}  re: ${hash.regionalQuakeList.length}`);
       return RSVP.hash(hash);
     });
   }
@@ -153,12 +148,33 @@ console.log(`end: ${end.isAfter(moment.utc(this.cache.cacheEnd).add(this.updateI
 
   loadOldLocal() {
     const that = this;
+    const data_host = dataHost();
     let loadingPromise;
     if (this.cache.oldLocal === null || this.cache.oldLocal.length === 0) {
-      loadingPromise = this.store.query('quake', this.createLocalQueryParams(this.netStart, this.oldLocalEnd))
-      .then(oldLocal => {
-        that.cache.oldLocal = oldLocal;
-        return oldLocal;
+//      let historicalUrl = this.historicalUrl.replace('eeyore.seis.sc.edu', dataHost());
+      let historicalUrl = `http://${dataHost()}${this.historicalUrl}`;
+
+      loadingPromise = seisplotjs.util.doFetchWithTimeout(historicalUrl)
+      .then(response => {
+          if (response.status === 200) {
+            return response.text();
+          } else {
+            throw new Error(`Status not successful: ${response.status}`);
+          }
+      }).then(function(rawXmlText) {
+        return new DOMParser().parseFromString(rawXmlText, seisplotjs.util.XML_MIME);
+      }).then(function(rawXml) {
+        return seisplotjs.quakeml.parseQuakeML(rawXml, data_host);
+      }).then(function(quake_array) {
+        let fdsnEventSerializer = new FdsnEventSerializer();
+        let cacheoldLocal = [];
+        for (let q of quake_array) {
+          let norm_q = fdsnEventSerializer.normalizeQuake(q);
+          let store_q = that.store.push(norm_q);
+          cacheoldLocal.push(store_q);
+        }
+        that.cache.oldLocal = cacheoldLocal;
+        return that.cache.oldLocal;
       });
     } else {
       loadingPromise = RSVP.resolve( this.cache.oldLocal );

fc73088f04764745eaeac50f14866f2800a94400
Philip Crotwell <crotwell@seis.sc.edu>
2024-01-12 17:56:16 -0500

    latency page initial

--- src/routes/latency/+page.svelte
+++ src/routes/latency/+page.svelte
@@ -1,6 +1,46 @@
 
 <script lang="ts">
   import NavBar from "$lib/navbar.svelte";
+  import { onMount } from 'svelte';
+
+  onMount(async () => {
+    const  sp  = await import('seisplotjs');
+    const datalatency = await import("$lib/data-latency.ts");
+
+    const latencyServ = new datalatency.DataLatencyService();
+
+    function updateLatency() {
+      latencyServ.queryLatency().then(ldata => {
+        const div = document.querySelector("div.stations");
+        while (div.lastChild) {
+          div.removeChild(div.lastChild);
+        }
+        div.appendChild(document.createElement("p")).textContent = `Got Latency: ${ldata.accessTime}`;
+      console.log(JSON.stringify(ldata.latestData[0]))
+        ldata.latestData.forEach(ld => {
+
+          div.appendChild(document.createElement("p")).textContent = `${ld.velocity.cloud} ${ld.velocity.iris} ${ld.velocity.eeyore}`;
+        });
+
+      });
+
+      setTimeout( () => {
+        updateLatency();
+      }, latencyServ.updateInterval.toMillis());
+    }
+    updateLatency();
+  });
 </script>
 
 <NavBar/>
+
+<h5 id="nowtime">Now!</h5>
+<div>
+  <sp-timerange duration="P2DT0M"></sp-timerange>
+  <button id="loadToday">Today</button>
+  <button id="loadNow">Now</button>
+</div>
+<div class="plot"></div>
+<div class="stations"></div>
+<div class="datakeys"></div>
+<div><pre class="raw"></pre></div>

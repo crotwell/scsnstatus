bdc9fd45afdb92c0dd4919680556f90d279653a9
Philip Crotwell <crotwell@seis.sc.edu>
2024-02-07 15:02:40 -0500

    add contact info

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -9,9 +9,13 @@
 
 <pre>
 School of Earth, Ocean and Environment
+University of South Carolina
 701 Sumter St.
 Columbia SC 20208
 </pre>
+<a href="mailto:scsn@seis.sc.edu">
+  scsn@seis.sc.edu
+</a>
 <pre>
 (803)777-4535
 </pre>

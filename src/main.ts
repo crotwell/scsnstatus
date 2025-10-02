f210980f548bd81cda2b981863c038546738e33e
Philip Crotwell <crotwell@seis.sc.edu>
2025-10-02 17:38:41 -0400

    add last month eq to main

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import './style.css'
 import * as sp from 'seisplotjs';
 import {createNavigation} from './navbar';
-import {loadActiveStations} from './util';
+import {loadActiveStations, SC_QUAKE_URL, SC_STATION_URL} from './util';
 
 createNavigation();
 const app = document.querySelector<HTMLDivElement>('#app')!
@@ -17,6 +17,10 @@ app.innerHTML = `
     fitbounds="false">
   </sp-station-quake-map>
 
+  <h3>Earthquakes in last month:</h3>
+  <sp-quake-table>
+  </sp-quake-table>
+
   <h5 class="spjsversion">Built using <a href="https://crotwell.github.io/seisplotjs/">spjs version</a>: ${sp.version}</h5>
 `
 }
@@ -26,4 +30,19 @@ map.quakeList = []
 loadActiveStations().then(staList => {
   map.addStation(staList);
   map.draw();
+  return staList;
+}).then(staList => {
+  const quakePromise = sp.quakeml.fetchQuakeML(SC_QUAKE_URL)
+    .then(qml => {
+      const monthAgo = sp.luxon.Interval.before(sp.luxon.DateTime.utc(),
+        sp.luxon.Duration.fromISO("P31DT0M"));
+      qml.eventList = qml.eventList.filter( q=> monthAgo.contains(q.time));
+      return qml;
+    });
+  return Promise.all([staList, quakePromise]);
+}).then( ([staList, qml] ) => {
+  map.quakeList = []
+  map.addQuake(qml.eventList);
+  map.draw();
+  document.querySelector("sp-quake-table").quakeList = qml.eventList;
 })
